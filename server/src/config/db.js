c42e6436e12c8e91f65a19e121a4f1f98c1ba212
Harsha <harshamahitha2079@gmail.com>
2026-06-23 16:41:19 +0530

    DB migration in User Schema because of adding name field

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,4 +2,25 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+const tenantId = 'a7865a05-e67b-4609-970d-aa8393b14204';
+const roleId = 'ee51907c-7cb3-4a0c-ad71-e6fe0e4ddba6';
+const userId = '83e763cd-4e5a-4b66-986e-49ea957dcce8';
+const email = 'venky@mowa.com';
+const passwordHash = 'venkymowa';
+
+async function seeding() {
+    // const allPermissions = await prisma.permission.findMany();
+
+    const data = await prisma.userRole.create({
+        data: {
+            userId,
+            roleId
+        }
+    });
+
+    console.log(data);
+}
+
+seeding();
+
 module.exports = prisma;
