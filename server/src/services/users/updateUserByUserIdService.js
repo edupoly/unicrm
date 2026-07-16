a54cc794fe28e8daa9d2ce6d51295d746b18419d
prudvi <patnamprudvinath2006@gmail.com>
2026-07-16 08:10:32 +0530

    rearranged updateData assignment

--- server/src/services/users/updateUserByUserIdService.js
+++ server/src/services/users/updateUserByUserIdService.js
@@ -3,13 +3,13 @@ import prismaDB from "../../config/database.js";
 const updateUserByUserIdService = async (tenantId, userId, data) => {
   const { name, mobileNumber, email, passwordHash, roleIds } = data;
 
-  const result = await prismaDB.$transaction(async (tx) => {
-    const updateData = {};
-    if (name !== undefined) updateData.name = name;
-    if (mobileNumber !== undefined) updateData.mobileNumber = mobileNumber;
-    if (email !== undefined) updateData.email = email;
-    if (passwordHash !== undefined) updateData.passwordHash = passwordHash;
+  const updateData = {};
+  if (name !== undefined) updateData.name = name;
+  if (mobileNumber !== undefined) updateData.mobileNumber = mobileNumber;
+  if (email !== undefined) updateData.email = email;
+  if (passwordHash !== undefined) updateData.passwordHash = passwordHash;
 
+  const result = await prismaDB.$transaction(async (tx) => {
     await tx.user.update({
       where: { id: userId, tenantId },
       data: updateData
